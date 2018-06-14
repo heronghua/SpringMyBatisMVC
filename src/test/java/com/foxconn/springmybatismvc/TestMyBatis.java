c9a1927879c58e5cad7be588ed5f10ee02f785b9
heronghua <heronghua1989@sina.com>
2018-06-14 23:24:28 +0800

    heronghua——添加测试包，添加css,img,js文件夹，修改mapperLocation 通配

--- src/test/java/com/foxconn/springmybatismvc/TestMyBatis.java
+++ src/test/java/com/foxconn/springmybatismvc/TestMyBatis.java
@@ -12,26 +12,17 @@ import com.alibaba.fastjson.JSON;
 import com.foxconn.springmybatismvc.service.UserService;
 import com.foxconn.springmybatismvc.vo.User;
 
-@RunWith(SpringJUnit4ClassRunner.class)     //��ʾ�̳���SpringJUnit4ClassRunner��  
+@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring-mybatis.xml"})  
   
 public class TestMyBatis {  
     private static Logger logger = Logger.getLogger(TestMyBatis.class);  
-//  private ApplicationContext ac = null;  
     @Resource  
     private UserService userService = null;  
   
-//  @Before  
-//  public void before() {  
-//      ac = new ClassPathXmlApplicationContext("applicationContext.xml");  
-//      userService = (IUserService) ac.getBean("userService");  
-//  }  
-  
     @Test  
     public void test1() {  
-        User user = userService.getUserByNu(1);  
-        // System.out.println(user.getUserName());  
-        // logger.info("ֵ��"+user.getUserName());  
+        User user = userService.getUserByNu(1);
         logger.info(JSON.toJSONString(user));
     }  
 }  
